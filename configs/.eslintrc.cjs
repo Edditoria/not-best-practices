cfb5ee487e5f1ec195ba142f5328829c5b8545e7
Edditoria <edditoria@pm.me>
2022-04-13 14:27:04 +0800

    Add eslint and prettier configs

---
+++ configs/.eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+		webextensions: true,
+		serviceworker: true,
+	},
+	extends: ['eslint:recommended', 'prettier'],
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	rules: {
+		'linebreak-style': ['error', 'unix'],
+		indent: ['error', 'tab'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'], // Default: 'always'.
+	},
+};
